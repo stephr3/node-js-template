efc65679c9e250d8c8b296e5bc2bbc6ae6a96a08
Stephanie Gurung <stephr3@gmail.com>
2016-07-30 11:14:56 -0700

    function using gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
@@ -6,8 +7,14 @@ gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
-gulp.task('jsBrowserify', function(){
-  return browserify({entries: ['./js/pingpong-interface.js']})
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function(){
+  return browserify({entries: ['./tmp/allConcat.js']})
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'))
