5aa4a4aca81f9ec1dae3300bee6144579d2747e8
Stephanie Gurung <stephr3@gmail.com>
2016-07-30 11:21:55 -0700

    minify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
 gulp.task('concatInterface', function(){
-  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
@@ -18,4 +19,10 @@ gulp.task('jsBrowserify', ['concatInterface'], function(){
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"))
 });
