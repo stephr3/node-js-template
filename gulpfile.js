8519a224c13e58b5dc444c3dc51ecb1dcd2ac8d1
Stephanie Gurung <stephr3@gmail.com>
2016-08-02 23:19:44 -0700

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
-
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var buildProduction = utilities.env.production;
 
 gulp.task('myTask', function(){
@@ -76,7 +77,8 @@ gulp.task('build', ['clean'], function(){
   } else {
     gulp.start('jsBrowserify');
   }
-  gulp.start('bower');
+  gulp.start('bower')
+  gulp.start('cssBuild');
 });
 
 gulp.task('serve', function(){
@@ -89,6 +91,7 @@ gulp.task('serve', function(){
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['scss/*.scss'], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -101,4 +104,13 @@ gulp.task('bowerBuild', ['bower'], function(){
 
 gulp.task('htmlBuild', function() {
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function(){
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
